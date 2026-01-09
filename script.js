ba56e8c96d7a0cfe0b954766d2b87e216ba9a590
thejaschandra <thejaschandra@gmail.com>
2026-01-09 10:05:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function showMessage() {
     document.getElementById("message").innerText =
-        "Your static site is successfully running on Render!";
+        "Teaching This Semester-> Cloud Computing and Security";
 }
+
